4f2bb1b3fda823861d5692064f36ef09a83db297
cuibaps1 <cuibaps1@users.noreply.github.com>
2018-09-18 00:10:19 -0700

    home page
    creating home http

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ app.get('/about',(req,res)=>{
     res.send('Player-Connect is a place for gamers to meet up and find people to play games with.');
 });
 
+app.get('/home',(req,res)=>{
+	res.send('This is the Player-Connect homepage.');
+});
+
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log(`listening on port ${port}...`);
