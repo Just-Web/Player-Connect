f6e65e838c30f5257b5387cb73e5a9422023ff07
cuibaps1 <cuibaps1@users.noreply.github.com>
2018-09-18 12:50:37 -0700

    add function HTTP profile
    add function HTTP profile

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to Player-Connect!!');
+app.get('/', function(req, res) {
+  res.send('Welcome to Player-Connect!!');
 });
 
-app.get('/about',(req,res)=>{
-    res.send('Player-Connect is a place for gamers to meet up and find people to play games with.');
+app.get('/about', function(req, res) {
+  res.send(
+    'Player-Connect is a place for gamers to meet up and find people to play games with.'
+  );
 });
 
-app.get('/home',(req,res)=>{
-	res.send('This is the Player-Connect homepage.');
+app.get('/home', function(req, res) {
+  res.send('This is the Player-Connect homepage.');
 });
 
-app.get('/accounts',(req,res)=>{
-	res.send('Create Account.');
+app.get('/profile/:id', function(req, res) {
+  res.send('you requested to see a profile with the id of ' + req.params.id);
 });
+
 const port = process.env.PORT || 3000
-app.listen(port, ()=>{
-    console.log(`listening on port ${port}...`);
-} );
+app.listen(port, function() {
+  console.log(`listening on port ${port}...`);
+});
