b269f54d24f46ed3c0bf4adc978abe3dbbf7bdc6
hunglay <hunglay0420@gmail.com>
2018-09-18 00:28:27 -0700

    update account page

--- server/index.js
+++ server/index.js
@@ -5,6 +5,17 @@ app.get('/',(req,res)=>{
     res.send('Welcome to Player-Connect!!');
 });
 
+app.get('/about',(req,res)=>{
+    res.send('Player-Connect is a place for gamers to meet up and find people to play games with.');
+});
+
+app.get('/home',(req,res)=>{
+	res.send('This is the Player-Connect homepage.');
+});
+
+app.get('/accounts',(req,res)=>{
+	res.send('Create Account.');
+});
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log(`listening on port ${port}...`);
