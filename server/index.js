f5ef6919822ea7a4e647ff9f9dfadee8862f646a
cuibaps1 <jadanngo@gmail.com>
2018-09-25 01:09:30 -0700

    test ejs
    ejs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine','ejs');
+
 var path = require('path');
 
 app.get('/', function(req, res) {
@@ -14,11 +17,13 @@ app.get('/home', function(req, res) {
   res.sendFile(path.resolve('../client/app/views/homepage.html'));
 });
 
-app.get('/profile/:name', function(req, res) {
-  res.send('you requested to see a profile with the name of ' + req.params.name);
+//dynamic content, using ejs template
+app.get('/profile/:id', function(req, res) {
+  var data = {age:29, job:'chef', hobbies:['eating', 'playing', 'climbing']};
+  res.render(path.resolve('../client/app/views/profile'), {id: req.params.id, data:data});
 });
 
-const port = process.env.PORT || 3000
+const port = 9000
 app.listen(port, function() {
   console.log(`listening on port ${port}...`);
 });
